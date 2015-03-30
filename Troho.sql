22df0cdca65e7d455413c56fa64e8d9c973c4550
Connor Kerns <ckerns@usc.edu>
2015-03-29 18:02:07 -0700

    Add initial sql scripts

---
+++ Troho.sql
@@ -0,0 +1,64 @@
+DROP DATABASE if exists Troho;
+
+CREATE DATABASE Troho;
+
+USE Troho;
+
+CREATE TABLE HousingLocations (
+  locationID int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  locationName varchar(50) NOT NULL,
+  textAddress varchar(70) NOT NULL,
+  description varchar(500) NOT NULL,
+  imageURLs varchar(500) NOT NULL,
+  floorplanURLs varchar(500), # currently don't require these
+  gpsLatitude varchar(10) NOT NULL, #TODO may want to change to a different identifier
+  gpsLongitude varchar(10) NOT NULL,
+  distanceToCampus varchar(10) NOT NULL,
+  # Average ratings so we don't recalculate every time the page is viewed
+  averageManagement varchar(4),
+  averageAmenities varchar(4),
+  averageLocation varchar(4),
+  averageNoise varchar(4),
+  averageCommunityChillFactor varchar(4),
+  averageRent varchar(4)
+);
+
+CREATE TABLE Users (
+  userID int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  userName varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  currentLocationID int(10),
+  facebookID varchar(50) NOT NULL, #TODO will probably need to change this
+  FOREIGN KEY (currentLocationID) REFERENCES HousingLocations(locationID)
+);
+
+CREATE TABLE Friends (
+  userID int(10) PRIMARY KEY NOT NULL,
+  friendID int(10) NOT NULL,
+  FOREIGN KEY (userID) REFERENCES Users(userID),
+  FOREIGN KEY (friendID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Surveys (
+  userID int(10) PRIMARY KEY NOT NULL,
+  question1 int(1) NOT NULL,
+  question2 int(1) NOT NULL,
+  question3 int(1) NOT NULL,
+  question4 int(1) NOT NULL,
+  question5 int(1) NOT NULL,
+  FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Reviews (
+  locationID int(10) PRIMARY KEY NOT NULL,
+  userID int(10) NOT NULL,
+  managementScore int(1) NOT NULL,
+  amenitiesScore int(1) NOT NULL,
+  locationScore int(1) NOT NULL,
+  noiseScore int(1) NOT NULL,
+  communityChillFactorScore int(1) NOT NULL,
+  rentPaid int(4), # add NOT NULL if we want to enforce them saying
+  timeWritten TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (locationID) REFERENCES HousingLocations(locationID),
+  FOREIGN KEY (userID) REFERENCES Users(userID)
+);
---
+++ TrohoTest.sql
@@ -0,0 +1,21 @@
+#now() for timestamp
+
+USE Troho;
+
+INSERT INTO HousingLocations (locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, distanceToCampus) VALUES ('First House', '123 30th St', 'A nice house.', 'path/to/images', 'path/to/floorplans', '0', '123', '10|12');
+INSERT INTO HousingLocations (locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, distanceToCampus) VALUES ('Second House', '124 30th St', 'A marginally worse house.', 'path/to/images', 'path/to/floorplans', '0', '123', '10|12');
+SELECT * FROM HousingLocations;
+
+INSERT INTO Users (userName, email, facebookID) VALUES ('Steve', 'asdf@asdf.asdf', '2weuhfdsu2j34f');
+INSERT INTO Users (userName, currentLocationID, email, facebookID) VALUES ('John', 2, 'asdf@asdf.asdf', '23rfsajkdf');
+SELECT * FROM Users;
+
+INSERT INTO Friends (userId, friendID) VALUES ('1', '2');
+INSERT INTO Friends (userId, friendID) VALUES ('2', '1');
+SELECT * FROM Friends;
+
+INSERT INTO Surveys (userID, question1, question2, question3, question4, question5) VALUES ('1', '10', '10', '5', '0', '0');
+SELECT * FROM Surveys;
+
+INSERT INTO Reviews (locationID, userID, managementScore, amenitiesScore, locationScore, noiseScore, communityChillFactorScore, timeWritten) VALUES ('1', '2', '5', '5', '5', '4', '5', now());
+SELECT * FROM Reviews;
