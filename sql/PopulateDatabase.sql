76ee2f9f6be75eeba74ea5d040aa6fee62f10408
arushs <arushshankar@gmail.com>
2015-04-22 20:31:18 -0700

    database file

--- sql/PopulateDatabase.sql
+++ sql/PopulateDatabase.sql
@@ -1,7 +1,9 @@
 USE Troho;
 
-INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'First House', '123 30th St', 'A nice house.', 'path/to/images', 'path/to/floorplans', '0', '123', 10, 12);
-INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'Second House', '124 30th St', 'A marginally worse house.', 'path/to/images', 'path/to/floorplans', '0', '123', 10, 12);
+INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'The Lorenzo', '325 W Adams Blvd', 'Luxurious apartments slightly far from USC campus.', './img/lorenzo.jpeg', 'path/to/floorplans', '0', '123', 10, 12);
+INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'Icon Plaza', '3584 S Figueroa St', 'Small apartment series close to campus', './img/iconplaza.jpeg', 'path/to/floorplans', '0', '123', 10, 12);
+INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'Tuscany', '3770 S. Figueroa St.', 'Apartments close to USC. Very big and quite Luxurious.', './img/tuscany.jpeg', 'path/to/floorplans', '0', '123', 10, 12);
+INSERT INTO HousingLocations (housingType, locationName, textAddress, description, imageURLs, floorplanURLs, gpsLatitude, gpsLongitude, minutesWalking, minutesBiking) VALUES (3, 'Gateway Apartments', '124 30th St', 'Most convenient apartment series. You get what you pay for.', './img/gateway.jpeg', 'path/to/floorplans', '0', '123', 10, 12);
 SELECT * FROM HousingLocations;
 
 INSERT INTO Users (userName, housingKey, email, facebookID, isAdmin, verifiedEmail) VALUES ('Steve', 1, 'steve@asdf.asdf', '2weuhfdsu2j34f1d4', false, true);
@@ -18,3 +20,6 @@ SELECT * FROM Surveys;
 
 INSERT INTO Reviews (housingKey, facebookID, textComment, managementScore, amenitiesScore, locationScore, noiseScore, communityChillFactorScore, timeWritten) VALUES ('1', '2weuhfdsu2j34f1d4', 'my comment', '5', '5', '5', '4', '5', now());
 SELECT * FROM Reviews;
+INSERT INTO Reviews (housingKey, facebookID, textComment, managementScore, amenitiesScore, locationScore, noiseScore, communityChillFactorScore, timeWritten) VALUES ('3', '2weuhfdsu2j34f1d4', 'I love Gateway, its awesome!', '5', '5', '5', '4', '5', now());
+
+
