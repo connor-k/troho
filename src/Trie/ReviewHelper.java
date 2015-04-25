9b49829596b86e2a66ff3c0eb481bcb869273e5e
Rachel Wood <rachelwood@usc-secure-wireless-061-086.usc.edu>
2015-04-24 17:13:40 -0700

    Fixing sql stuff

--- src/Trie/ReviewHelper.java
+++ src/Trie/ReviewHelper.java
@@ -7,7 +7,7 @@ import sql.HousingLocation;
 import sql.Review;
 
 public class ReviewHelper {
-	Vector<Review> pruneReviews(String houseName) {
+	Vector<Review> pruneReviews(String houseName, boolean [] tags) {
 		HousingLocation location = HousingDataManager.getHousingLocation(houseName);
 		Review [] reviewArray = location.reviews;
 		Vector<Review> reviews = new Vector<Review>();
@@ -18,5 +18,11 @@ public class ReviewHelper {
 		return reviews;
 	}
 	
-	//boolean checkValidReview(Review review, )
+	boolean checkValidReview(Review review, boolean []tags) {
+		//boolean reviewTags = review.
+		for(int i = 0; i < 6; i++) {
+			//if()
+		}
+		return true;
+	}
 }
