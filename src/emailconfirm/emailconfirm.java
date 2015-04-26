79696f2cb75113dc3cf9cd900366c580caf5d6b8
Vita <vshubin@usc.edu>
2015-04-26 07:07:39 -0700

    =emailssss

--- src/emailconfirm/emailconfirm.java
+++ src/emailconfirm/emailconfirm.java
@@ -4,65 +4,6 @@ import sql.UserDataManager;
 
 import org.apache.commons.mail.*;
 
-import java.util.UUID; 
-
-public class emailconfirm {
-	private static String toConfirm; //address that needs to be confirmed 
-	private static String facebookID;  //users unique facebook identification 
-	private static String key; 		//unique confirmation key 
-	private static String message; 	//body of email 
-	
-	public emailconfirm(String to, String fbID) throws EmailException
-	{
-		toConfirm = to; 
-		facebookID = fbID; 
-		key = java.util.UUID.randomUUID().toString();	//generates random key for indiv user
-		sendEmail();
-		UserDataManager.setValidationKey(facebookID, key); 
-	}
-	
-	public static void sendEmail() throws EmailException 
-	{
-		Email email = new SimpleEmail();
-		email.setHostName("smtp.googlemail.com");
-		email.setSmtpPort(465);
-		email.setAuthentication("troho.troco", "trohofor201");
-		email.setSSLOnConnect(true);
-		email.setFrom("troho.troco@gmail.com");
-		email.setSubject("Troho Confirmation");
-		message = "Welcome to troho by troco! Please confirm your email. "
-				+ "\n \n Please confirm your email here: "
-				+ "\n localhost:8080/troho/Validation?key=" + key + "&id=" + facebookID; 
-		email.setMsg(message); 
-		email.addTo(toConfirm);
-		email.send();
-	}
-	
-	public static void sendTestEmail(String code, String id)
-	{
-		try {
-			new emailconfirm("vitashubin@hotmail.com", "111");
-			System.out.println(message);
-		} catch (EmailException e) {
-			e.printStackTrace();
-		} 
-	}
-	
-	public static void emailConfirmed(String code, String id)
-	{
-		UserDataManager.verifyEmail(id, key);
-		//for testing purposes
-		System.out.println("User ID " + id + " confirmed with key " + code); 
-	}
-
-}
-=======
-package emailconfirm;
-
-import sql.UserDataManager;
-
-import org.apache.commons.mail.*;
-
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.URLDataSource;
