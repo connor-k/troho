0f990291f52c9ae4a2198ec19bccc83e5046f766
arushs <arushshankar@gmail.com>
2015-04-26 02:35:56 -0700

    Fixing servlet

--- src/servlet/SearchFilter.java
+++ src/servlet/SearchFilter.java
@@ -30,8 +30,72 @@ public class SearchFilter extends HttpServlet {
 		
 		response.setContentType("application/json");
 		JSONObject object = requestParamsToJSON(request);
-//		JSONArray elements = object.names();
-//		object = new JSONObject(elements.getString(0));
+		JSONArray elements = object.names();
+		object = new JSONObject(elements.getString(0));
+		String searchWords = object.getString("searchWords");
+		int managementScore = object.getInt("managementScore");
+		int amenitiesScore = object.getInt("amenitiesScore");
+		int locationScore = object.getInt("locationScore");
+		int noiseScore = object.getInt("noiseScore");
+		int communityChillFactorScore = object.getInt("communityChillFactorScore");
+		int maxPrice = object.getInt("maxPrice");
+		int maxDistance = object.getInt("maxDistance");
+		String housingType =  object.getString("housingType");
+		int minRating = object.getInt("minRating");
+		
+		SearchHelper mySearch = new SearchHelper();
+		
+		//APARTMENT, DORM, HOUSE
+		boolean isHouse = false;
+		boolean isApartment = false;
+		boolean isDorm = false;
+		//ousingType.valueOf(Apartment)
+		
+		if(housingType.equals("Apartment")) {
+			isApartment = true;
+		} else if (housingType.equals("House")) {
+			isHouse = true;
+		} else { 
+			isDorm = true;
+		}
+		
+		HousingLocation [] myHouses;
+		myHouses = mySearch.sortPruneHouses(managementScore, amenitiesScore, locationScore, noiseScore, 
+				communityChillFactorScore, searchWords, maxPrice, maxDistance, isHouse, isDorm, 
+				isApartment, minRating);
+		
+		PrintWriter out = response.getWriter();
+		
+		JSONObject searchObject = new JSONObject();
+		JSONArray searchArray = new JSONArray();
+		
+		for(int i = 0; i < myHouses.length; i++) {
+			JSONObject currObject = new JSONObject();
+			{
+				currObject.put("locationName", myHouses[i].locationName);
+				currObject.put("imageURL", myHouses[i].imageURL);
+				currObject.put("housingAddress", myHouses[i].address);
+				currObject.put("price", "$" + myHouses[i].averageRent);
+				currObject.put("distance", "" + myHouses[i].distanceToCampus);
+				currObject.put("housingType", housingType);
+				currObject.put("rating", myHouses[i].overallScore);
+				
+			}
+			searchArray.put(i, currObject);
+		}
+		
+		searchObject.put("searchArray", searchArray);
+		
+		out.print(searchObject);
+		out.flush();
+	}
+	
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("application/json");
+		JSONObject object = requestParamsToJSON(request);
+		JSONArray elements = object.names();
+		object = new JSONObject(elements.getString(0));
 		String searchWords = object.getString("searchWords");
 		int managementScore = object.getInt("managementScore");
 		int amenitiesScore = object.getInt("amenitiesScore");
