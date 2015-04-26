864dea141ffb794c47221c7a05de58ef66223a97
CalvinLeGassick <calvin.legassick@gmail.com>
2015-04-26 07:08:54 -0700

    number of reviews servlet

---
+++ src/NumberOfReviews.java
@@ -0,0 +1,53 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sql.HousingDataManager;
+
+/**
+ * Servlet implementation class NumberOfReviews
+ */
+@WebServlet("/NumberOfReviews")
+public class NumberOfReviews extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NumberOfReviews() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		BufferedReader br = request.getReader();
+		String houseName = br.readLine();
+		
+		int number =  HousingDataManager.getHousingLocation(houseName).reviews.length;
+		
+		PrintWriter pr = new PrintWriter(response.getWriter());
+		
+		pr.print(HousingDataManager.getHousingLocation(houseName).reviews.length);
+		pr.flush();
+	}
+
+}
